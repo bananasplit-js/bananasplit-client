0e971ad0509d6ccc73e76b27a034a1a1f9476282
Diego Ulloa <diegoulloao@icloud.com>
2020-10-04 22:37:13 -0300

    Preparing v1

--- src/cli.js
+++ src/cli.js
@@ -1,24 +1,30 @@
 /**
  * 
- *  Banana Client
- *  v1.0.3
- *  @diegoulloao
+ *  Bananasplit client
+ *  @version 1.0.0
  * 
- *  2020 · Apache Licence 2.0
+ *  @author diegoulloao
+ *  @license Apache 2.0
+ * 
+ *  2020
  * 
  */
-
 import arg from 'arg'
 import shell from 'shelljs'
 import chalk from 'chalk'
+import boxen from 'boxen'
 
 import { version } from './../package.json'
 
-function parseArgumentsIntoOptions( rawArgs ) {
+
+function parseArgumentsIntoOptions ( rawArgs ) {
+
     const args = arg(
         {
             '--git': Boolean,
             '-g': '--git',
+            '--apollo': Boolean,
+            '-a': '--apollo',
             '--version': Boolean,
             '-v': '--version'
         },
@@ -29,52 +35,98 @@ function parseArgumentsIntoOptions( rawArgs ) {
 
     return {
         git: args['--git'] || false,
+        apollo: args['--apollo'] || false,
         action: args._[0],
-        project: args._[1],
+        project_name: args._[1],
         version: args['--version'] || false
     }
+
 }
 
-async function createBananaProject( options ) {
-    if ( options.action ) {
-        switch ( options.action ) {
+
+function createBananaProject ({ action, project_name, apollo, git }) {
+
+    if ( action ) {
+
+        switch ( action ) {
             case 'new':
-                if ( options.project ) {
-                    const clone = await shell.exec( `git clone https://github.com/diegoulloao/bananasplit-express-template.git ${options.project}` )
+                if ( project_name ) {
+                    console.log()
+
+                    const branch = !apollo ? 'main/template':'main/apollo'
+                    const clone = shell.exec( `git clone -b ${branch} https://github.com/bananasplit-js/bananasplit-js ${project_name}` )
 
                     if ( clone.code === 0 ) {
-                        await shell.rm([ `${options.project}/README.md`, `${options.project}/LICENSE` ])
-                        await shell.rm( '-rf', `${options.project}/.git` )
-                        await shell.rm( '-rf', `${options.project}/.github` )
+                        shell.rm([ `${project_name}/README.md`, `${project_name}/LICENSE` ])
+                        shell.rm( '-rf', `${project_name}/.git` )
+                        shell.rm( '-rf', `${project_name}/.github` )
 
-                        if ( options.git ) {
-                            await shell.exec( `cd ${options.project} && git init` )
-                        }
+                        if ( git )
+                            shell.exec( `cd ${project_name} && git init` )
+                        ;
 
-                        console.log( chalk.bgYellow.black(`${options.project} created!`) )
+                        let infoBox = []
+                        
+                        // Message output
+                        infoBox.push( chalk.bold.green('New project!') )
+                        infoBox.push( '\n\n' )
+                        infoBox.push( `${ chalk.white('- Name      →') }  ${ chalk.yellow(project_name) }` )
+                        infoBox.push( '\n' )
+                        infoBox.push( `${ chalk.white('- Template  →') }  ${ chalk.cyanBright( !apollo ? 'express':'express + apollo') }` )
+                        infoBox.push( `\n${ chalk.white('- Git       →') }  ${ chalk[ git ? 'green':'red' ]( git ? 'yes':'no' ) }` )
+                        infoBox.push( '\n\n' )
+                        infoBox.push( chalk.gray(`Location: ${ shell.pwd().toString() }/${project_name }`) )
+
+                        // Info box
+                        console.log( boxen(infoBox.join(''), {
+                            padding: 1,
+                            margin: 1,
+                            borderColor: 'yellow',
+                            borderStyle: 'bold'
+                        }) )
+
                     } else {
-                        console.log( chalk.bgYellow.black('Project not created.') )
+
+                        console.log( `\n${chalk.bgRed.white(` ${chalk.bold.white('Error:')} The project could not be created `)}` )
                         break
                     }
-                } else
-                    console.log( chalk.bgWhite.black('Need specify a project name.') )
+
+                } else {
+                    console.log( chalk.bgYellow.black('\n Need to specify a project name ') )
+                }
 
                 break
+            ;
             
+
             default:
-                console.log( chalk.bgWhite.black(`Action "${options.action}" not valid.`) )
+                console.log( chalk.bgRed.white(`\n ${chalk.bold(action)} is not an action ` ) )
+            ;
+
         }
+
     } else {
-        console.log( chalk.bgWhite.black('Action required.') )
+        console.log( chalk.bgRed.white('\n Action is required ') )
     }
+
 }
 
-export function cli( args ) {
+export function cli ( args ) {
+
+    // Exit when no git installed
+    if ( !shell.which('git') ) {
+        console.log( chalk.bgRed.white(`\n This client requires ${chalk.bold.white('git')} `) )
+        return ;
+    }
+
+
     let options = parseArgumentsIntoOptions( args )
     
-    if ( options.version ) {
-        console.log( chalk.bgYellow.black(version) )
-    } else {
+    if ( options.version )
+        console.log( `\n${chalk.bgCyan.black(` Bananasplit Client v${version.substr(0, 3)} `)}` )
+
+    else
         createBananaProject( options )
-    }
+    ;
+
 }
